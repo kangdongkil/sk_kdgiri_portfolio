e5d5519ff64c2ea4663ae880dcde5747c04d4f2f
kangdongkil <kbk3418@naver.com>
2016-10-12 03:30:04 +0900

    이미지 수정, 리스트 슬라이드 추가

--- js/list_animation.js
+++ js/list_animation.js
@@ -1,19 +1,14 @@
 jQuery(function($){
-//    $('.on_list> li').on('click',function(event){event.preventDefault();
-//    var src = $(this).find('a>img').attr('src');
-//    var filename = src.match(/[^\/]+$/)[0];
-//    $('.on_list>li').attr('src',bigSrc);
-//                                            });
-//    $('.on_list li').click(function(e){
-//	e.preventDefault();
-//	var img = $(this).find('img');
-//	if(img.attr('src').indexOf('off') >= 0){
-//		img.attr('src', img.attr('src').replace('off','on'));	
-//	}else{
-//		img.attr('src', img.attr('src').replace('on','off'));
-//	}
-//}).mouseover(function(){
-//        war
-//    })
-//    
+    //리스트 드롭앤 드랍
+    $(function(){
+       
+		$('.main_list>li').on('mouseenter',function(){	 
+            $(this).find('ul').slideDown('slow');
+        });
+		$('.main_list>li').on('mouseleave',function(){		
+			 $(this).find('ul').slideUp('slow');	
+            
+		});
+		
+	});
 });
