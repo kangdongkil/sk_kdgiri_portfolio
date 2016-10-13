06b08932324e8b206b780cf5f1a156662aaeb07b
kangdongkil <kbk3418@naver.com>
2016-10-14 03:27:10 +0900

    리스트 애니메이션 오류 수정

--- js/list_animation.js
+++ js/list_animation.js
@@ -2,11 +2,12 @@ jQuery(function($){
     //리스트 드롭앤 드랍
     $(function(){
        
-		$('.main_list>li').on('mouseenter',function(){	 
-            $(this).find('ul').slideDown('slow');
-        });
+		$('.main_list>li').on('mouseenter',function(){
+            $(this).find('ul').stop().slideDown('slow');
+            });
+         
 		$('.main_list>li').on('mouseleave',function(){		
-			 $(this).find('ul').slideUp('slow');	
+			 $(this).find('ul').stop().slideUp('slow');	
             
 		});
 		
